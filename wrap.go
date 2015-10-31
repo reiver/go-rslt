cac46716e6da183d028ae7bc14195d7a64dbbe68
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-10-31 11:47:40 -0700

    added some comments.

--- wrap.go
+++ wrap.go
@@ -9,6 +9,16 @@ import (
 // Wrap returns a Result.
 func Wrap(args ...interface{}) Result {
 
+	// Loop through all the arguments passed to this func.
+	//
+	// If there is an error in the arguments, the returned rslt.Result
+	// is a rslt.Error.
+	//
+	// Else, the first non-error, non-Warning, non-nil (of any kind)
+	// argument (if it exists) is the rslt.Value.
+	//
+	// Else. the returned rslt.Result is a rslt.Nil.
+
 	var value   interface{}
 	var warning Warning
 
